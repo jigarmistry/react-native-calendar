05cf3aa993fe8a44b8042e36d71f3c0f2ea5d58f
Jigar Mistry <jigarmistry24@gmail.com>
2017-10-30 11:09:59 +0530

    Update calendar background color

--- components/styles.js
+++ components/styles.js
@@ -2,7 +2,7 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   calendarContainer: {
-    backgroundColor: '#f7f7f7',
+    backgroundColor: '#ffffff',
   },
   monthContainer: {
   },
